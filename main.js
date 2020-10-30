7a011d2d72838a7d48a1eb4af429be866a3b3c32
Naveen <navi.mehta@gmail.com>
2020-10-30 16:28:41 +1100

    updated styles

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function handleUpdateRoute(event) {
     } 
 }
 
-// train logic
+// train logic below
 
 const trainLines = [
     ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
@@ -40,10 +40,12 @@ const trainLines = [
     ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
 ]
 
-var originTrainLine = 0;
-var destinationTrainLine = 0;
+var originTrainLine = 0
+var destinationTrainLine = 0
 var finalRouteOriginLine = []
 var finalRouteDestinationLine = []
+var originLine = null
+var destinationLine = null
 
 function handleCheckRoute(origin, destination) {
     originLine = handleCheckLine(origin)
@@ -57,23 +59,36 @@ function handleCheckRoute(origin, destination) {
     let richmondIndexDestination = trainLines[destinationLine].indexOf("Richmond")
 
     // push origin line stations in the route to final array for origins  
-    if (stationIndexInOriginLine < richmondIndexOrigin) {
-        for (let i = stationIndexInOriginLine; i <= richmondIndexOrigin; i++) {
-            finalRouteOriginLine.push(trainLines[originLine][i])
-        }
+    if (originLine == destinationLine) {
+        if (stationIndexInOriginLine < stationIndexInDestinationLine) { 
+            for (let i = stationIndexInOriginLine; i <= stationIndexInDestinationLine; i++) {
+                finalRouteOriginLine.push(trainLines[originLine][i])
+            }        
+        } else {
+            for (let i = stationIndexInOriginLine; i >= stationIndexInDestinationLine; i--) {
+                finalRouteOriginLine.push(trainLines[originLine][i])
+            }
+        }        
     } else {
-        for (let i = stationIndexInOriginLine; i >= richmondIndexOrigin; i--) {
-            finalRouteOriginLine.push(trainLines[originLine][i])
+        if (stationIndexInOriginLine < richmondIndexOrigin) {
+            for (let i = stationIndexInOriginLine; i <= richmondIndexOrigin; i++) {
+                finalRouteOriginLine.push(trainLines[originLine][i])
+            }
+        } else {
+            for (let i = stationIndexInOriginLine; i >= richmondIndexOrigin; i--) {
+                finalRouteOriginLine.push(trainLines[originLine][i])
+            }
         }
-    }
-    // push the destination line stations in the route to final array for destinations 
-    if (stationIndexInDestinationLine < richmondIndexDestination) {
-        for (let i = richmondIndexDestination; i >= stationIndexInDestinationLine; i--) {
-            finalRouteDestinationLine.push(trainLines[destinationLine][i])
-        }
-    } else {
-        for (let i = richmondIndexDestination; i <= stationIndexInDestinationLine; i++) {
-            finalRouteDestinationLine.push(trainLines[destinationLine][i])
+
+        // push the destination line stations in the route to final array for destinations 
+        if (stationIndexInDestinationLine < richmondIndexDestination) {
+            for (let i = richmondIndexDestination; i >= stationIndexInDestinationLine; i--) {
+                finalRouteDestinationLine.push(trainLines[destinationLine][i])
+            }
+        } else {
+            for (let i = richmondIndexDestination; i <= stationIndexInDestinationLine; i++) {
+                finalRouteDestinationLine.push(trainLines[destinationLine][i])
+            }
         }
     }
 
@@ -99,14 +114,31 @@ function handleCheckStationIndex(station, line) {
 
 // returns the train line containing the origin or destination
 function handleCheckLine(station) {
-    for (let i = 0; i < trainLines.length; i++) {
-        if (trainLines[i].includes(station)) {
-            if (i == 0) {
-                return 0; //alamein
-            } else if (i == 1) {
-                return 1; // glen waverly
-            } else {
-                return 2; // sandingham
+    if (station === "Richmond" && destinationLine == null && originLine !== null) {
+        return originLine
+    } else if (station === "Richmond" && destinationLine == null && originLine == null) {
+        // check destination line
+        for (let i = 0; i < trainLines.length; i++) {
+            if (trainLines[i].includes(destination)) {
+                if (i == 0) {
+                    return 0; //alamein
+                } else if (i == 1) {
+                    return 1; // glen waverly
+                } else {
+                    return 2; // sandingham
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < trainLines.length; i++) {
+            if (trainLines[i].includes(station)) {
+                if (i == 0) {
+                    return 0; //alamein
+                } else if (i == 1) {
+                    return 1; // glen waverly
+                } else {
+                    return 2; // sandingham
+                }
             }
         }
     }
@@ -114,16 +146,12 @@ function handleCheckLine(station) {
 
 // print final route
 function handleDisplayFinalRoute(arr1, arr2) {
-    
-    // calculate stops
-    var stops = arr1.length + arr2.length - 2
-
-    if (arr2 == undefined) {
-        // var displayFinalRoute = arr1[0]
+    if (arr2.length == 0) {
+        // calculate stops
+        var stops = arr1.length + arr2.length - 1
         for (let i = 0; i < arr1.length - 1; i++) {
             arr1[i] = arr1[i]+" ----->"
         }
-        
         arr1 = arr1.join(" ") 
         document.querySelector("#display-origin").textContent = `Origin: ${origin}`
         document.querySelector("#display-destination").textContent = `Destination: ${destination}`
@@ -131,26 +159,22 @@ function handleDisplayFinalRoute(arr1, arr2) {
         document.querySelector("#display-total-stops").textContent = `${stops} stops total`
 
     } else {
-        
         for (let i = 0; i < arr1.length - 1; i++) {
             arr1[i] = arr1[i]+" ----->"
         }
-        
         for (let i = 0; i < arr2.length - 1; i++) {
             arr2[i] = arr2[i]+" ----->"
         }
-
         var arr1StringLength = 0
         for (let i = 0; i < arr1.length - 1; i++) {
             arr1StringLength += arr1[i].length
         }
-
         // calculate empty space 
         let emptySpace = ' '.repeat(arr1StringLength + 5) 
-
+        // calculate stops
+        var stops = arr1.length + arr2.length - 2
         arr1 = arr1.join(" ") 
         arr2 = arr2.join(" ") 
-
         document.querySelector("#display-origin").textContent = `Origin: ${origin}`
         document.querySelector("#display-destination").textContent = `Destination: ${destination}`
         document.querySelector("#display-route-1").textContent = arr1
